74b0ba5b84afed1623cfbc1b5f7af448f6281bba
Sajjad Karimi <50092872+sajjadkarimi91@users.noreply.github.com>
2022-08-11 13:28:03 +0430

    Update eeg_preprocessing.m

--- 1 load-preprocess data/eeg_preprocessing.m
+++ 1 load-preprocess data/eeg_preprocessing.m
@@ -1,7 +1,9 @@
 clear
 clc
 
-%This code is dependent on eeglab functions, and eeglab must be integrated with the biomag toolbox
+%This code is dependent on eeglab functions, and eeglab must insatll and
+%add biosig plugin
+
 TimesVar = 5;
 OutlierPercentage = 0.025;
 
@@ -11,7 +13,7 @@ mkdir(save_dir);
 
 
 for i = 1:9
-
+    %% EEGLAB functions
     EEG = pop_biosig([dataset_dir,'/A0',num2str(i),'T.gdf']);
     clc
     [~ , EEG.data] = sjk_outlier_clip( EEG.data , OutlierPercentage , TimesVar );
@@ -20,7 +22,6 @@ for i = 1:9
 
     %% EOG Correction Sameni Method
     EOG_channels = 23:25 ;
-
     fs = 125 ;
     eeg_signals = double ( EEG.data  );
     [ andis_EOG, andis_WEOG,EEGDatapower] = eog_detection_intervals( eeg_signals ,fs, EOG_channels ) ;
@@ -43,16 +44,16 @@ for i = 1:9
     %ERP extraction for component (769 Left hand) (770 Right hand) (771 foot) (772 tongue)
     %EEG_ALL(1).EEG = pop_epoch( EEG , {  '769'  }, [ -4.5   4.5], 'newname', ' resampled resampled epochs epochs', 'epochinfo', 'yes'); % old command
 
-    EEG_ALL(1).EEG = pop_epoch( EEG , {  'class1, Left hand	- cue onset (BCI experiment)'  }, [ -4.5   4.5], 'newname', ' resampled resampled epochs epochs', 'epochinfo', 'yes');
+    EEG_ALL(1).EEG = pop_epoch( EEG , { 'class1, Left hand	- cue onset (BCI experiment)'  }, [ -4.5   4.5], 'newname', ' resampled resampled epochs epochs', 'epochinfo', 'yes');
 
-    EEG_ALL(2).EEG = pop_epoch( EEG , {  'class2, Right hand	- cue onset (BCI experiment)'  }, [ -4.5   4.5], 'newname', ' resampled resampled epochs epochs', 'epochinfo', 'yes');
+    EEG_ALL(2).EEG = pop_epoch( EEG , { 'class2, Right hand	- cue onset (BCI experiment)'  }, [ -4.5   4.5], 'newname', ' resampled resampled epochs epochs', 'epochinfo', 'yes');
 
-    EEG_ALL(3).EEG = pop_epoch( EEG , {  'class3, Foot, towards Right - cue onset (BCI experiment)'  }, [ -4.5   4.5], 'newname', ' resampled resampled epochs epochs', 'epochinfo', 'yes');
+    EEG_ALL(3).EEG = pop_epoch( EEG , { 'class3, Foot, towards Right - cue onset (BCI experiment)'  }, [ -4.5   4.5], 'newname', ' resampled resampled epochs epochs', 'epochinfo', 'yes');
 
-    EEG_ALL(4).EEG = pop_epoch( EEG , {  'class4, Tongue		- cue onset (BCI experiment)'  }, [ -4.5   4.5], 'newname', ' resampled resampled epochs epochs', 'epochinfo', 'yes');
+    EEG_ALL(4).EEG = pop_epoch( EEG , { 'class4, Tongue		- cue onset (BCI experiment)'  }, [ -4.5   4.5], 'newname', ' resampled resampled epochs epochs', 'epochinfo', 'yes');
 
     save([save_dir,'/A0',num2str(i),'T.mat'],'EEG_ALL');
 
 end
 
-make_channel_location;
+make_channel_location; % create channel loaction file for future plotting EEG sensors on the scalp
